07f8043ce14903a1b191c30cb67a33b344b71c9b
foti <footios76@gmail.com>
2022-09-24 21:50:43 +0300

    Add sub-routes and use Outlet

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/myapps" element={<Navigate replace to={"/learn"} />} />
+      <Route path="/learn" element={<Learn />}>
+        <Route path="courses" element={<Courses />} />
+        <Route path="bundles" element={<Bundles />} />
+      </Route>
     </Routes>
   </Router>
 );
@@ -21,4 +33,42 @@ function Home() {
     </div>
   );
 }
+
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
+      <Link className="btn btn-success" to="/learn/courses">
+        courses
+      </Link>
+      <br />
+      <br />
+      <br />
+      <Link className="btn btn-primary" to="/learn/bundles">
+        bundle
+      </Link>
+      <Outlet />
+    </div>
+  );
+}
+
+function Courses() {
+  return (
+    <div>
+      <h1>Courses list</h1>
+      <h4>Course card</h4>
+    </div>
+  );
+}
+
+function Bundles() {
+  return (
+    <div>
+      <h1>Bundles list</h1>
+      <h4>Bundle card</h4>
+    </div>
+  );
+}
+
 reportWebVitals();
